dc6668f8b9c97b29650ae3bd616e9f90d6efe83c
Ryan Munger <ryanmunger@gmail.com>
2017-02-02 10:35:48 -0800

    Prevent scrolling on mobile when nav is open

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -5,9 +5,13 @@ var closeNav = document.querySelector('.close');
 navToggle.addEventListener('click', function(e) {
   nav.className = '';
   document.body.className = 'nav-open';
+  document.ontouchmove = function(e){
+    e.preventDefault();
+  }
 });
 
 closeNav.addEventListener('click', function(e) {
   nav.className = 'mobile-hidden';
   document.body.className = '';
+  document.ontouchmove = function(e) {}
 });
