3d21143a7ac92be6310a4eeb6b44d36224569bd9
Ryan Munger <ryanmunger@gmail.com>
2017-02-02 09:48:14 -0800

    Update mobile nav

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,13 @@
-(function($) {
-  $('#nav-toggle').on('click', function(e) {
-    e.preventDefault();
-    $('#nav').toggleClass('mobile-hidden');
-  });
-})(jQuery);
+var navToggle = document.getElementById('nav-toggle');
+var nav = document.getElementById('nav');
+var closeNav = document.querySelector('.close');
+
+navToggle.addEventListener('click', function(e) {
+  nav.className = '';
+  document.body.className = 'nav-open';
+});
+
+closeNav.addEventListener('click', function(e) {
+  nav.className = 'mobile-hidden';
+  document.body.className = '';
+});
