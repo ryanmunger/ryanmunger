f8c49a7ae7b9e00007b9b39219163d1b03fa6483
Ryan Munger <ryanmunger@gmail.com>
2017-02-02 15:44:23 -0800

    Add header photo on mobile

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,6 +1,9 @@
 var navToggle = document.getElementById('nav-toggle');
 var nav = document.getElementById('nav');
 var closeNav = document.querySelector('.close');
+var wrapper = document.querySelector('.wrapper');
+var mobileHeadshot = document.querySelector('.mobile-headshot');
+var mobileHeadshotHeight = getComputedStyle(mobileHeadshot).getPropertyValue('height');
 
 navToggle.addEventListener('click', function(e) {
   nav.className = '';
@@ -14,4 +17,21 @@ closeNav.addEventListener('click', function(e) {
   nav.className = 'mobile-hidden';
   document.body.className = '';
   document.ontouchmove = function(e) {}
+});
+
+wrapper.style.paddingTop = mobileHeadshotHeight;
+
+window.addEventListener('resize', function(e) {
+  var mobileHeadshotHeight = getComputedStyle(mobileHeadshot).getPropertyValue('height');
+  if (window.innerWidth < 768) {
+    wrapper.style.paddingTop = mobileHeadshotHeight;
+  } else {
+    wrapper.style.paddingTop = 0;
+  }
+});
+
+window.addEventListener('scroll', function(e) {
+  if (document.body.scrollTop < 0) {
+    mobileHeadshot.style.transform = 'scale(' + (1 + (Math.abs(document.body.scrollTop) / 100)) / 1 + ')';
+  }
 });
