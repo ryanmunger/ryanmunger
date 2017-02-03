02a5f479de6fa248ebf934266bb70876364616da
Ryan Munger <ryanmunger@gmail.com>
2017-02-02 17:13:28 -0800

    Add heroku deployment to travis

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -35,3 +35,5 @@ window.addEventListener('scroll', function(e) {
     mobileHeadshot.style.transform = 'scale(' + (1 + (Math.abs(document.body.scrollTop) / 100)) / 1 + ')';
   }
 });
+
+console.log('');
