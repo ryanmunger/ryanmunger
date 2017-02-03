3888d24eb9067866cd9fd7154c0db972728a0af3
Ryan Munger <ryanmunger@gmail.com>
2017-02-03 12:18:24 -0800

    Remove travis way of setting new relic key and try heroku env var

--- newrelic-key.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('newrelic.js', (err, data) => {
-  if (err) console.log(err);
-  const dataString = data.toString();
-  const result = dataString.replace('@@NEW_RELIC_KEY', process.env.NEW_RELIC_KEY);
-  fs.writeFileSync('newrelic.js', result);
-});
